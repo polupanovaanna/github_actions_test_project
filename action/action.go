9566a2f46b4c2f8470ad4c1c0431d221787837cc
polupanovaanna <ania.polupanova@mail.ru>
2023-03-06 11:08:21 +0100

    tmp

---
+++ action/action.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"project/util"
+)
+
+func CheckErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf(msg, err)
+	}
+}
+
+func main() {
+
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	//TODO ngrock ??
+
+	CheckErr(err, "did not connect: %s")
+
+	defer conn.Close()
+
+	c := util.NewCommitDataClient(conn)
+
+	masterHeadCommit := os.Getenv("INPUT_MASTERHEADHASH")
+	triggerCommit := os.Getenv("INPUT_TRIGGERHASH")
+
+	/*refIter, _ := r.References()
+
+	refIter.ForEach(func(ref *plumbing.Reference) error {
+
+		if ref.Name().IsRemote() {
+			branchCommit, _ := r.CommitObject(ref.Hash())
+			patch, _ := masterHeadCommit.Patch(branchCommit)
+
+			response, err := c.Translate(context.Background(), &util.CommitInfo{HeadHash: masterHeadRef.Hash().String(),
+				CommitDiff: patch.String()})
+			CheckErr(err, "Error when translating info to server: %s")
+			fmt.Println(response)
+			fmt.Println("branch: ", patch.String())
+		}
+		return nil
+	}) //iterating branches
+
+	//response, err := c.Translate(context.Background(), &util.CommitInfo{HeadHash: masterHeadRef.String()})
+
+	CheckErr(err, "Error when processing git info: %s")
+
+	//log.Printf("Response from server: %s", response)*/
+
+}
